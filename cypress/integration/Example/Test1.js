bdb6c7d7ac783958c77e1a3628355425b5585606
MahnoorLuqman <mahnoorluqman.irl@gmail.com>
2023-06-05 20:57:21 +0100

    Code Added

---
+++ cypress/integration/Example/Test1.js
@@ -0,0 +1,15 @@
+/// <reference types="Cypress" />
+describe('my first test suit',function(){
+
+it('my first test case', function(){
+
+    cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/')
+    cy.get('.search-keyword').type("cucumber")
+    cy.get('.search-button').click()
+})
+
+})
+
+
+
+
